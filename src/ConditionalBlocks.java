85e701daa67ef03cc5926cea5ff3d91264fa1457
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-16 17:15:44 -0400

    create ElseIf_Statements class

--- src/ConditionalBlocks.java
+++ src/ConditionalBlocks.java
@@ -2,20 +2,32 @@
 public class ConditionalBlocks {
 
 	public static void main(String[] args) {
-		boolean b = false;
-		
-		if(b) {
-			System.out.println("Inside if statement");
-		}
-		System.out.println("Outside if statement");
-		
+		boolean b = true;
 		b = false;
 		
+		
 		if(b) {
-			System.out.println("Inside if statement after reasignment to false");
+			System.out.println("Inside the if-statement after reasignment to false");
+		} else {
+			System.out.println("Inside the else-statement after reasignment to false");
 		}
-		System.out.println("Outside if statement after reasignment to false");
-
+		System.out.println("Outside of the if-statement");
+		
+	    boolean firstCondition = false;
+	    boolean secondCondition = true;
+	    
+	    System.out.println("-----------------------------------------------------------------");
+	    
+	    if(firstCondition) {
+	    	System.out.println("Inside the if-statement");
+	    	
+	    	if(secondCondition) {
+	    		System.out.println("Inside the nested if-statement");
+	    	}
+	    } else {
+	    	System.out.println("Inside inside the else-statement");
+	    }
+	    System.out.println("Outisde of the second if-statement");
 	}
 
 }
